7970e0e88b4caea01441089c7701dd261ad62b4d
goyalkaruna <goyalkaruna@yahoo.co.in>
2014-10-12 13:06:23 -0700

    Cmpe275Lab2

---
+++ src/main/java/com/my275assign/sample/HomeController.java
@@ -0,0 +1,36 @@
+package com.my275assign.sample;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+
+
+public class HomeController extends AbstractController{
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest arg0,
+			HttpServletResponse arg1) throws Exception {
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		return new ModelAndView("home", "dateDisplay", formattedDate);
+	}
+	
+	
+}
